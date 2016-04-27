3e939ca737e38e03e27594e8e09d5d6e2fe2a404
Ayush Saraswat <saraswatayu@gmail.com>
2016-04-27 15:46:40 -0700

    Add finished view.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # StackView
-A quick endeavor into stack views.
+A quick endeavor into stack views. Watch the webcast on Youtube at https://www.youtube.com/watch?v=01JAZEnDLgA.
+
+## 
+Developed by Ayush Saraswat for an iOS Webcast on Stack Views.
---
+++ StackView/StackView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackView
+//
+//  Created by Ayush Saraswat on 4/27/16.
+//  Copyright © 2016 Udacity, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
